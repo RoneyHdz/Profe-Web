650705993a5e4c7542c51528bbb298df510b82c2
Roney martinez <RoneyHdz>
2024-06-07 11:53:12 -0600

    Resta

---
+++ Resta.py
@@ -0,0 +1,9 @@
+def restar_numeros(a, b):
+    return a - b
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+resultado = restar_numeros(numero1, numero2)
+
+print(f"La resta de {numero1} y {numero2} es {resultado}")
