c5dc2db1648015bde7b0f54b118546e0f9a5e584
Roney martinez <RoneyHdz>
2024-06-07 11:48:10 -0600

    Inicio de proyecto

---
+++ suma.py
@@ -0,0 +1,12 @@
+def sumar_numeros(a, b):
+    return a + b
+
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+
+resultado = sumar_numeros(numero1, numero2)
+
+
+print(f"La suma de {numero1} y {numero2} es {resultado}")
