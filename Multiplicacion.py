a553cfc4cb08de6a36a875209ed9e904cbb33f2d
Roney martinez <RoneyHdz>
2024-06-07 11:57:59 -0600

    Multiplicacion

---
+++ Multiplicacion.py
@@ -0,0 +1,9 @@
+def multiplicar_numeros(a, b):
+    return a * b
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+resultado = multiplicar_numeros(numero1, numero2)
+
+print(f"La multiplicación de {numero1} y {numero2} es {resultado}")
